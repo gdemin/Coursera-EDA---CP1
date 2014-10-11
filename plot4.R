c437c9ab99984326727dae2e089096823103aafc
Gregory Demin <gdemin+github@gmail.com>
2014-10-12 03:01:05 +0400

    Final plot4.

---
+++ plot4.R
@@ -0,0 +1,72 @@
+################################################################################
+### script for building plot4.png from Coursera EDA Course Project 1 ############
+################################################################################
+
+# you should change path according to ypur system
+setwd("C:/Users/gregory/Documents/!Projects/Trainings/Coursera - Exploratory Data Analysis/EDA-CP1")
+
+# Data was downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# on 08.10.2014. I consider it too big to put it on GitHub.
+
+# Unzip file
+unzipped_file = unzip("exdata-data-household_power_consumption.zip")
+
+# read data
+power_cons_data = read.table(unzipped_file,header=TRUE,sep=";",na.strings = "?",stringsAsFactors = FALSE)
+
+unlink (unzipped_file) # delete unzipped file
+
+# take subset of dataset
+power_cons_data = power_cons_data[power_cons_data$Date %in% c('1/2/2007','2/2/2007'),]
+
+# convert characters to datetime
+power_cons_data$datetime = strptime(paste(power_cons_data$Date,power_cons_data$Time),format="%d/%m/%Y %H:%M:%S")
+
+# Set locale for correct (US) weekdays abbreviation
+Sys.setlocale("LC_TIME", "us")
+
+# Plot data on png device to avoid legend cut-off. 
+png(filename="plot4.PNG", width = 480, height = 480)
+
+# Change graphics parameter for 4 plots on single window.
+par(mfrow=c(2,2))
+
+plot(power_cons_data$datetime,power_cons_data$Global_active_power,
+     type="l",
+     xlab = "",
+     ylab = "Global Active Power",
+     cex.lab = 0.9
+)
+
+plot(power_cons_data$datetime,power_cons_data$Voltage,
+     xlab ="datetime",
+     ylab = "Voltage", 
+     type="l",
+     cex.lab = 0.9
+)
+
+plot(power_cons_data$datetime,power_cons_data$Sub_metering_1,
+     type="l",
+     xlab = "",
+     ylab = "Energy sub metering",
+     cex.lab = 0.9
+     )
+lines(power_cons_data$datetime,power_cons_data$Sub_metering_2,col="red")
+lines(power_cons_data$datetime,power_cons_data$Sub_metering_3,col="blue")
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       bty="n",
+       lty=1,
+       col=c("black","red","blue")
+       )
+
+plot(power_cons_data$datetime,power_cons_data$Global_reactive_power,
+     xlab ="datetime",
+     ylab = "Global_reactive_power", 
+     type="l",
+     cex.lab = 0.9
+)
+
+dev.off()
+
+
